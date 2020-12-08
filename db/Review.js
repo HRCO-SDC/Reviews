de88bc6a2ccc5f0e3ef0230c4a1e1b89ad2fb346
CCBaxter84 <troyceclark@gmail.com>
2020-12-07 17:11:09 -0700

    installed mongoose and faker and completed Review.js

---
+++ db/Review.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  rating: { type: Number, required: true },
+  recommend: { type: Boolean, required: true },
+  response: { type: String, required: true },
+  body: { type: String, required: true },
+  date: { type: Date, required: true },
+  reviewer_name: { type: String, required: true },
+  helpfulness: { type: Number, required: true },
+  reported: { type: Boolean, required: true },
+  product_id: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Reviews', reviewSchema);
