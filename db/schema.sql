3ee3014fb342b9e51cc0ce19a0767726b16052f4
CCBaxter84 <troyceclark@gmail.com>
2020-12-08 21:45:07 -0700

    Added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE if not exists reviews2 (
+  review_id serial PRIMARY KEY,
+  rating INTEGER,
+  recommend BOOLEAN,
+  response VARCHAR(255),
+  body VARCHAR(255),
+  date TIMESTAMPTZ,
+  reviewer_name VARCHAR(255),
+  helpfulness INTEGER,
+  reported BOOLEAN,
+  product_id INTEGER
+);
+
+\copy reviews(rating, recommend, response, body, date, reviewer_name, helpfulness, reported, product_id) FROM '/Users/mywork/Documents/coding_projects/work/sdc/Reviews/db/all.csv' WITH DELIMITER ',' CSV HEADER;
